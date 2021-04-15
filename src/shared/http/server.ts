e3b50da561fd855dcb9490e47ea4301a9b2239fe
José Oliveira <j.oliveira90@live.com>
2021-04-15 16:02:38 -0300

    add AppError middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import routes from '../http/routes/index';
+import routes from './routes/index';
+import AppError from '../errors/AppError';
 
 const app = express();
 
@@ -8,4 +9,18 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+// @ts-ignore
+app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+// eslint-disable-next-line no-console
 app.listen(3333, () => console.log('SERVER RUNNING! 🏆'));
