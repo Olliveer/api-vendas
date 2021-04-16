69c5697d21584ef3523501d8054842220af00a32
José Oliveira <j.oliveira90@live.com>
2021-04-16 15:36:14 -0300

    add ProductController

---
+++ src/modules/products/controllers/ProductController.ts
@@ -0,0 +1,62 @@
+/* eslint-disable class-methods-use-this */
+import { Request, Response } from 'express';
+import CreateProductService from '../services/CreateProductService';
+import DeleteProductService from '../services/DeleteProductService';
+import ListProductService from '../services/ListProductService';
+import ShowProductService from '../services/ShowProductService';
+import UpdateProductService from '../services/UpdateProductService';
+
+class ProductsController {
+  async index(req: Request, res: Response) {
+    const listProducts = new ListProductService();
+
+    const products = await listProducts.execute();
+
+    return res.status(200).json(products);
+  }
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const showProduct = new ShowProductService();
+
+    const product = await showProduct.execute({ id });
+
+    return res.status(200).json(product);
+  }
+
+  async create(req: Request, res: Response) {
+    const { name, price, quantity } = req.body;
+
+    const createProduct = new CreateProductService();
+
+    const product = await createProduct.execute({ name, price, quantity });
+
+    return res.status(200).json(product);
+  }
+
+  async update(req: Request, res: Response) {
+    const { name, price, quantity } = req.body;
+    const { id } = req.params;
+
+    const updateProduct = new UpdateProductService();
+
+    const product = await updateProduct.execute({
+      id, name, price, quantity,
+    });
+
+    return res.status(200).json(product);
+  }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const deleteProduct = new DeleteProductService();
+
+    await deleteProduct.execute({ id });
+
+    return res.status(200);
+  }
+}
+
+export default ProductsController;
