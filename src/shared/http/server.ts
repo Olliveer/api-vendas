78e0228c04c9e0989166814f857320e885b2b741
José Oliveira <j.oliveira90@live.com>
2021-04-17 14:12:05 -0300

    add celebrate and validation routes on ProductsRoutes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '../errors/AppError';
 import '../typeorm';
@@ -11,12 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 // @ts-ignore
 // eslint-disable-next-line no-unused-vars
 app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
+      status: 'error',
       message: error.message,
     });
   }
