d83db9c9cbaf191fef9b0aed8e13485b72aa37e7
José Oliveira <j.oliveira90@live.com>
2021-04-18 14:31:43 -0300

    add CreateUserService

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '../../../shared/errors/AppError';
+import UserRepository from '../repositories/UsersRepository';
+import User from '../typeorm/entities/User';
+
+interface IRequestUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  // eslint-disable-next-line class-methods-use-this
+  async execute({ name, email, password }: IRequestUser): Promise<User> {
+    const usersRepository = getCustomRepository(UserRepository);
+
+    const emailExists = usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Eamil address already used.');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
