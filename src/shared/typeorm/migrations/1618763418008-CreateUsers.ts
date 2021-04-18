44fdf1f6fc4de3164d160944b6430d48ec58f136
José Oliveira <j.oliveira90@live.com>
2021-04-18 13:37:17 -0300

    create migration users

---
+++ src/shared/typeorm/migrations/1618763418008-CreateUsers.ts
@@ -0,0 +1,53 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/prefer-default-export */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1618763418008 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
