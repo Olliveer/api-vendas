bd18bce8f350db5908aac12d11561a1b56c515a6
José Oliveira <j.oliveira90@live.com>
2021-04-27 11:56:24 -0300

    add UserController

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,30 @@
+/* eslint-disable class-methods-use-this */
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import ListUserService from '../services/ListUserService';
+
+class UsersController {
+  async index(req: Request, res: Response): Promise<Response> {
+    const listUser = new ListUserService();
+
+    const users = await listUser.execute();
+
+    return res.json(users);
+  }
+
+  async create(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return res.json(user);
+  }
+}
+
+export default UsersController;
