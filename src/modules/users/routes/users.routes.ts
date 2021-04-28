269156fcf49b0b980bd9cac28b70934a2f957d93
José Oliveira <j.oliveira90@live.com>
2021-04-28 16:19:08 -0300

    static route to frontend files

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,17 @@
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
 import { Router } from 'express';
+import uploadConfig from '../../../config/upload';
 import isAuthenticate from '../../../shared/http/middlewares/isAuthenticate';
 import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', isAuthenticate, usersController.index);
 
@@ -16,5 +22,7 @@ usersRouter.post('/', celebrate({
     password: Joi.string().required(),
   },
 }), usersController.create);
+
+usersRouter.patch('/avatar', isAuthenticate, upload.single('avatar'), usersAvatarController.update);
 
 export default usersRouter;
