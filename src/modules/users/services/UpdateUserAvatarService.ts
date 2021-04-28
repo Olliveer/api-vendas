e7188cc2d47f41d13aa64ecc7820446cb4d56261
José Oliveira <j.oliveira90@live.com>
2021-04-28 15:56:01 -0300

    update avatar service

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,43 @@
+/* eslint-disable camelcase */
+import path from 'path';
+import { getCustomRepository } from 'typeorm';
+import fs from 'fs';
+import AppError from '../../../shared/errors/AppError';
+import UserRepository from '../repositories/UsersRepository';
+import User from '../typeorm/entities/User';
+import uploadConfig from '../../../config/upload';
+
+interface IRequestUser {
+  user_id: string;
+  avatarFileName: string;
+}
+
+class UpdateUserAvatarService {
+  // eslint-disable-next-line class-methods-use-this
+  async execute({ user_id, avatarFileName }: IRequestUser): Promise<User> {
+    const usersRepository = getCustomRepository(UserRepository);
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
