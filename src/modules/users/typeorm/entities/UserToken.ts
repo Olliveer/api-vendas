10fec896c486dd4d1e86d2073a49b7b088e580fc
José Oliveira <j.oliveira90@live.com>
2021-04-29 16:39:46 -0300

    add UserToken entity

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+import {
+  Column, CreateDateColumn, Entity, Generated, PrimaryColumn, UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('user_tokens')
+class UserToken {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export default UserToken;
