abd74791341694759de8ac7c4906cebc846432b5
José Oliveira <j.oliveira90@live.com>
2021-04-29 16:36:54 -0300

    add UserTokens migration

---
+++ src/shared/typeorm/migrations/1619724028968-CreateUsersTokens.ts
@@ -0,0 +1,61 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/prefer-default-export */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsersTokens1619724028968 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_tokens',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'token',
+            type: 'uuid',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKTokenUser',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_tokens');
+  }
+}
