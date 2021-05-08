2da0f4cc9d4ef7e3dfdaecb2a18c10b20fd423fc
José Oliveira <j.oliveira90@live.com>
2021-05-08 16:19:53 -0300

    add user profile routes

---
+++ src/modules/users/controllers/ProfileController.ts
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+/* eslint-disable class-methods-use-this */
+import { Request, Response } from 'express';
+import ShowProfileService from '../services/ShowProfileService';
+import UpdateProfileService from '../services/UpdateProfileService';
+
+class ProfileController {
+  async show(req: Request, res: Response): Promise<Response> {
+    const showProfile = new ShowProfileService();
+    const user_id = req.user.id;
+
+    const user = await showProfile.execute({ user_id });
+
+    return res.json(user);
+  }
+
+  async update(req: Request, res: Response): Promise<Response> {
+    const {
+      name, email, password, old_password,
+    } = req.body;
+    const user_id = req.user.id;
+
+    const showProfile = new UpdateProfileService();
+
+    const user = await showProfile.execute({
+      user_id, name, email, password, old_password,
+    });
+
+    return res.json(user);
+  }
+}
+
+export default ProfileController;
