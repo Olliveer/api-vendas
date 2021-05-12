290d9ddf61367f228fcecc3c00ea4776b2be1d20
José Oliveira <j.oliveira90@live.com>
2021-05-12 13:45:39 -0300

    add typeorm pagination

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,6 +1,18 @@
+/* eslint-disable camelcase */
 import { getCustomRepository, Repository } from 'typeorm';
 import Customer from '../typeorm/entities/Customer';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
 
 class ListCustomersService {
   private customersRepository: Repository<Customer>;
@@ -9,10 +21,10 @@ class ListCustomersService {
     this.customersRepository = getCustomRepository(CustomersRepository);
   }
 
-  public async execute(): Promise<Customer[]> {
-    const customers = await this.customersRepository.find();
+  public async execute(): Promise<IPaginateCustomer> {
+    const customers = await this.customersRepository.createQueryBuilder().paginate();
 
-    return customers;
+    return customers as IPaginateCustomer;
   }
 }
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
+import { pagination } from 'typeorm-pagination';
 import routes from './routes';
 import AppError from '../errors/AppError';
 import uploadConfig from '../../config/upload';
@@ -12,11 +13,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(pagination);
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors());
 
-// @ts-ignore
 // eslint-disable-next-line no-unused-vars
 app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
   if (error instanceof AppError) {
