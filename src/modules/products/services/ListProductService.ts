f1d8baede5942a43c272cd8af5e9aabb34140d96
José Oliveira <j.oliveira90@live.com>
2021-05-13 16:54:49 -0300

    add redis cache

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,13 +1,18 @@
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import ProductRepository from '../typeorm/repositories/ProductRepository';
+import RedisCache from '../../../shared/cache/RedisCache';
 
 class ListProductService {
   // eslint-disable-next-line class-methods-use-this
   public async execute(): Promise<Product[]> {
     const productRepository = getCustomRepository(ProductRepository);
 
+    const redisCache = new RedisCache();
+
     const products = await productRepository.find();
+
+    await redisCache.save('text', 'text');
 
     return products;
   }
---
+++ src/shared/cache/RedisCache.ts
@@ -0,0 +1,25 @@
+/* eslint-disable class-methods-use-this */
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '../../config/cache';
+
+class RedisCache {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  public async save(key: string, value: any): Promise<void> {
+    await this.client.set(key, JSON.stringify(value));
+  }
+
+  // public async recover<T>(key: string): Promise<T | null> {
+
+  // }
+
+  // public async invalidate(key: string): Promise<void> {
+
+  // }
+}
+
+export default RedisCache;
