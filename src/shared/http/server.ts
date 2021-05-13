4cf6f776c79b49848b7cbc7380680a507f4bfd3a
José Oliveira <j.oliveira90@live.com>
2021-05-13 17:28:37 -0300

    rate limiter middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -9,11 +9,15 @@ import routes from './routes';
 import AppError from '../errors/AppError';
 import uploadConfig from '../../config/upload';
 import '../typeorm';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(rateLimiter);
+
 app.use(pagination);
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
