b26545a04b3ce245cfc33cb3e65aec53914aa807
José Oliveira <j.oliveira90@live.com>
2021-05-15 16:19:54 -0300

    config sendMailForgot to SESMail

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,6 +5,8 @@ import EtherealMail from '../../../config/mail/EtherealMail';
 import AppError from '../../../shared/errors/AppError';
 import UserRepository from '../typeorm/repositories/UsersRepository';
 import UserTokenRepository from '../typeorm/repositories/UserTokenRepository';
+import SESMail from '../../../config/mail/SESMail';
+import mailConfig from '../../../config/mail/mail';
 
 interface IRequestUser {
   email: string;
@@ -25,6 +27,24 @@ class SendForgotPasswordEmailService {
     const token = await userTokenRepository.generate(user.id);
 
     const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
+
+    if (mailConfig.driver === 'ses') {
+      await SESMail.sendMail({
+        to: {
+          name: user.name,
+          email: user.email,
+        },
+        subject: '[API VENDAS] Recuperação de senha',
+        templateData: {
+          file: forgotPasswordTemplate,
+          variables: {
+            name: user.name,
+            link: `${process.env.APP_WEB_URL}/password/reset?token=${token.token}`,
+          },
+        },
+      });
+      return;
+    }
 
     await EtherealMail.sendMail({
       to: {
